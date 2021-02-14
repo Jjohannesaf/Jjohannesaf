e288dc06d6b0a8ddb31cc8d2a31c5967f07fd53e
Johannes ahola <n9ahjo00@students.oamk.fi>
2021-02-14 19:30:11 +0200

    ja

--- App.js
+++ App.js
@@ -19,9 +19,10 @@ export default function App() {
     } else {
       result = (bottles*11.88 - ((weight/10)*time))/(weight*0.6);
     }
-     if (result <0){
-       return;
-     }
+    
+    // if (result <0){
+    //     return;
+    //   }
     
 
     setPromilles(result);
